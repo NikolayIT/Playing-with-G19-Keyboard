34e04aa584716e23096e255fe3eb74d932e57b7b
Nikolay Kostov <admin@nikolay.it>
2015-04-26 15:03:26 +0300

    Added text to the TestApplet

--- G19-Test-App/G19-Test-App/TestApplet.cs
+++ G19-Test-App/G19-Test-App/TestApplet.cs
@@ -2,23 +2,24 @@
 {
     using System;
     using System.Drawing;
+    using System.Drawing.Drawing2D;
     using System.Drawing.Imaging;
 
     public delegate void BitmapChangedHandler(object sender, BitmapChangedEventArgs e);
 
     public class TestApplet : IAppletProgram
     {
-        private Bitmap bitmap;
+        private readonly Bitmap bitmap;
+
+        private readonly Graphics graphics;
 
         public TestApplet()
         {
             this.bitmap = new Bitmap(320, 240, PixelFormat.Format32bppArgb);
-        }
-
-        public void Initialize()
-        {
-            this.bitmap.SetPixel(100, 100, Color.Yellow);
-            this.BitmapChangedInvoke();
+            this.graphics = Graphics.FromImage(this.bitmap);
+            this.graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            this.graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            this.graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
         }
 
         public event BitmapChangedHandler BitmapChanged;
@@ -29,6 +30,12 @@
             {
                 return "Nikolay.IT";
             }
+        }
+
+        public void Initialize()
+        {
+            this.graphics.DrawString("Hello, world from Nikolay.IT!", new Font("Consolas", 12), Brushes.White, new PointF(20, 20));
+            this.BitmapChangedInvoke();
         }
 
         private void BitmapChangedInvoke()
