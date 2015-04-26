5d479a771a49bd037d9e3127794019d36f3f35fd
Nikolay Kostov <admin@nikolay.it>
2015-04-26 13:56:52 +0300

    Created G19-Test-App empty console project

---
+++ G19-Test-App/G19-Test-App/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G19_Test_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
